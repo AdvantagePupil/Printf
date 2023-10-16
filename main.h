27b2624fb364d2ea5ebdb7b3cb855537484159fa
Swiftpixie <er.rumalak@gmail.com>
2023-10-16 09:23:18 -0700

    team

--- main.h
+++ main.h
@@ -25,8 +25,8 @@ int print_character(va_list args);
 int print_string(va_list args);
 int print_percentage(void);
 int print_37(void);
-int print_dec(va_list args)
-int print_int(va_list args)
+int print_dec(va_list args);
+int print_int(va_list args);
 
 /** string length */
 int _stringlen(char *str);
